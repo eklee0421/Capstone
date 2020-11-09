548587f190a273cbca3e5f210197b21e77ff2485
Lee EunKyung <ekyi0421@gmail.com>
2020-11-09 23:48:18 +0900

    프레그먼트 및 상단탭 추가

--- Capstone/app/src/main/java/org/techtown/capstone/WeatherActivity.java
+++ Capstone/app/src/main/java/org/techtown/capstone/WeatherActivity.java
@@ -1,14 +1,57 @@
 package org.techtown.capstone;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("현재 날씨"));
+        tabs.addTab(tabs.newTab().setText("도로 상황"));
+        tabs.addTab(tabs.newTab().setText("실시간 날씨"));
+
+        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+                Log.d("WeatherActivity", "선택된 탭: " + position);
+                Fragment selected = null;
+                if(position == 0) selected = fragment1;
+                else if(position == 1) selected = fragment2;
+                else if(position == 2) selected = fragment3;
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
